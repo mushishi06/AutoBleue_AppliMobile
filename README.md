783f5a2bfdb1a803817ab78884dc232a454872ac
belia-bourgeois <belia.bourgeois.romain@gmail.com>
2014-03-11 11:34:28 +0100

    update version 0.1.75 versionCode 01
    Changelog 0.1.75 :
    "update checkConnection function in js/autoBleueStart.js"
    "remove js/demo.js"

--- README.md
+++ README.md
@@ -7,13 +7,18 @@ The application download is averable on "https://build.phonegap.com/apps/576646/
 
 
 =============
-Changelog 0.1.74 :
-"remove Toast.js from www/js folder"
-"remove the social div from the menu"
-"add the swaper menu with cars, contact, tarif sections in info-swiper.html + Update the menu"
-"update in info-swiper.html the link click to call AutoBleue in contact section"
-"update maps plugin"
-"correction open-menu-left onClick on wing-icon (top-left)"
 
-Changelog 0.1.73 :
-"Add new fonctionnality : local storage"
+Changelog 0.1.75 : <br/>
+	"update checkConnection function in js/autoBleueStart.js"
+	"remove js/demo.js"
+	
+Changelog 0.1.74 : <br/>
+	"remove Toast.js from www/js folder"
+	"remove the social div from the menu"
+	"add the swaper menu with cars, contact, tarif sections in info-swiper.html + Update the menu"
+	"update in info-swiper.html the link click to call AutoBleue in contact section"
+	"update maps plugin"
+	"correction open-menu-left onClick on wing-icon (top-left)"
+
+Changelog 0.1.73 : <br/>
+	"Add new fonctionnality : local storage"
--- www/js/autoBleueStart.js
+++ www/js/autoBleueStart.js
@@ -172,7 +172,11 @@ function checkConnection() {
     states[Connection.CELL]     = 'Cell generic connection';
     states[Connection.NONE]     = 'No network connection';
 
-    alert('Connection type: ' + states[networkState]);
+	if (networkState == Connection.CELL || networkState == Connection.NONE) {
+		alert('Connection type: ' + states[networkState]);
+		return (false);
+	}
+	return (true);
 }
 	
 function updateSnapper() {
@@ -183,3 +187,30 @@ function updateSnapper() {
 	});
 	document.addEventListener("menubutton", onMenuKeyDown, false);
 }
+
+
+//previus in demo.js but not work ??
+// var addEvent = function addEvent(element, eventName, func) {
+	// if (element.addEventListener) {
+    	// return element.addEventListener(eventName, func, false);
+    // } else if (element.attachEvent) {
+        // return element.attachEvent("on" + eventName, func);
+    // }
+// };
+
+// addEvent(document.getElementById('open-left'), 'click', function(){
+	// snapper.open('left');
+// });
+
+/* Prevent Safari opening links when viewing as a Mobile App */
+(function (a, b, c) {
+    if(c in b && b[c]) {
+        var d, e = a.location,
+            f = /^(a|html)$/i;
+        a.addEventListener("click", function (a) {
+            d = a.target;
+            while(!f.test(d.nodeName)) d = d.parentNode;
+            "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href)
+        }, !1)
+    }
+})(document, window.navigator, "standalone");
